33390d6da4e18ccc9e949a035e7cc9ea563b44e3
Anders Blom <andersblom@anderss-mbp.home>
2019-07-20 07:45:00 -0500

    Add API call to login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 import LoginForm from 'components/LoginForm/LoginForm';
 
 const LoginIndex: React.FunctionComponent = () => {
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        axios
+            .post('http://finance-app-api.test/api/login', {
+                email: e.target[0].value,
+                password: e.target[1].value,
+            })
+            .then(res => console.log(res));
         console.dir(e.target[0].value, e.target[1].value);
     };
     return (
