c637f7943e0a5bec48973a56c478dd041f02675e
Anders Blom <andersblom@Anderss-MBP.localdomain>
2019-07-22 18:13:35 -0500

    Add .env support

--- next.config.js
+++ next.config.js
@@ -1,12 +1,27 @@
+require ('dotenv').config ();
 const path = require ('path');
+const Dotenv = require ('dotenv-webpack');
 
 module.exports = {
   webpack (config) {
+    config.plugins = config.plugins || [];
+
     config.resolve.alias['actions'] = path.join (__dirname, 'actions');
     config.resolve.alias['components'] = path.join (__dirname, 'components');
     config.resolve.alias['containers'] = path.join (__dirname, 'containers');
     config.resolve.alias['lib'] = path.join (__dirname, 'lib');
     config.resolve.alias['reducers'] = path.join (__dirname, 'reducers');
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv ({
+        path: path.join (__dirname, '.env'),
+        systemvars: true,
+      }),
+    ];
+
     return config;
   },
 };
