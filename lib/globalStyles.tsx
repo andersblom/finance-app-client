8c8f891adc224dcd83c8409c9b6cd1a2f27489f1
Anders Blom <andersblom@anderss-mbp-4.home>
2019-07-26 17:44:01 -0500

    Style lint fix issues + ignored files

--- lib/globalStyles.tsx
+++ lib/globalStyles.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'lib/styled-components';
 const GlobalStyles = createGlobalStyle`
     html, body, #__next {
         margin: 0;
-        padding 0;
+        padding: 0;
         height: 100%;
         width: 100%;
         background-color: ${props => props.theme.color.background};
